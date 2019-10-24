b7824c78269b71c5553e9f7a58f3aa871fa3e75d
Sania117 <saniapinnock116@gmail.com>
2019-10-24 03:28:05 +0000

    Finally made the first character of a word move to the end next use an array to make sure that that only happens to constants

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
     $("#button").click(function(){
-        function pig_latin(str) {
-            var vowels = ["a","e","i","o","u"];
-            if(vowels.indexOf(strcharAt(0)) !== -1){}
 
-        while(vow.indexOf(str.charAt(0)) == -1){
-        str = str.substr(1) + str.charAt(0);
-        }
-        return str += "ay";
-        }
+        let input = $(".input").val();
+       // $(".output").append("<div class = 'test'>" + input + "</div>");
+
+        let word = input.substr(1);
+
+
+        let index = input.charAt(0);
+
+        let pig = word.concat(index);
+
+        $(".output").append("<div class = 'test'>" + pig + "ay" + "</div>");
+
+
+
     });
 
 
